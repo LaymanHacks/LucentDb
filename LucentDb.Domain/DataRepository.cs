1a35fa4cc0237515b9a27d733cc307f2ea5f36f3
Mark Layman <MarkLayman@cox.net>
2015-05-30 13:54:51 -0500

    Add DataRepository

---
+++ LucentDb.Domain/DataRepository.cs
@@ -0,0 +1,53 @@
+﻿using LucentDb.Data.DbCommandProvider;
+using LucentDb.Data.Repository;
+using LucentDb.Data.SqlDbCommandProvider;
+
+namespace LucentDb.Domain
+{
+  public class DataRepository
+    {
+
+
+
+         public DataRepository()
+         {
+             ConnectionRepository = new DbConnectionRepository(new SqlDbConnectionCommandProvider());
+             ExpectedResultRepository = new DbExpectedResultRepository(new SqlDbExpectedResultCommandProvider());
+             ProjectRepository = new DbProjectRepository(new SqlDbProjectCommandProvider());
+             ProjectConnectionRepository = new DbProject_ConnectionRepository( new SqlDbProject_ConnectionCommandProvider());
+             RunHistoryRepository = new DbRunHistoryRepository(new SqlDbRunHistoryCommandProvider());
+             ScriptRepository = new DbScriptRepository( new SqlDbScriptCommandProvider());
+             ScriptTypeRepository = new DbScriptTypeRepository(new SqlDbScriptTypeCommandProvider());
+             TestRepository = new DbTestRepository( new SqlDbTestCommandProvider());
+             TestTypeRepository = new DbTestTypeRepository(new SqlDbTestTypeCommandProvider() );
+             TestScriptRepository = new DbTest_ScriptRepository( new SqlDbTest_ScriptCommandProvider());
+             AssertTypeRepository = new DbAssertTypeRepository(new SqlDbAssertTypeCommandProvider());
+         }
+
+      public ITestTypeRepository TestTypeRepository { get; set; }
+
+      public IAssertTypeRepository AssertTypeRepository { get; set; }
+
+      public ITest_ScriptRepository TestScriptRepository { get; set; }
+
+      public ITestTypeRepository DbTestTypeRepository { get; set; }
+
+      public ITestRepository TestRepository { get; set; }
+
+      public IScriptTypeRepository ScriptTypeRepository { get; set; }
+
+      public IScriptRepository ScriptRepository { get; set; }
+
+      public IRunHistoryRepository RunHistoryRepository { get; set; }
+
+      public IProject_ConnectionRepository ProjectConnectionRepository { get; set; }
+
+      public IProjectRepository ProjectRepository { get; set; }
+
+      public IExpectedResultRepository ExpectedResultRepository { get; set; }
+
+      public IConnectionRepository ConnectionRepository { get; set; }
+
+
+    }
+}
