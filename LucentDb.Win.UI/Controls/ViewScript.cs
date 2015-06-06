17802a133692938353f971c19c6d56055820534f
Mark Layman <Mark.l.layman@gmail.com>
2015-06-05 19:59:27 -0500

    Add script group cs files

---
+++ LucentDb.Win.UI/Controls/ViewScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ICSharpCode.TextEditor.Document;
+using LucentDb.Domain.Entities;
+
+namespace LucentDb.Win.UI.Controls
+{
+    public partial class ViewScript : UserControl
+    {
+
+        private Script _script;
+
+        public ViewScript()
+        {
+            
+            InitializeComponent();
+        }
+
+        public ViewScript(Script script)
+        {
+            _script = script;
+            InitializeComponent();
+            txtScriptValue.Document.HighlightingStrategy = HighlightingManager.Manager.FindHighlighter("SQL");
+
+            BindData();
+
+        }
+
+        private void BindData()
+        {
+            txtScriptValue.Text = _script.ScriptValue;
+            lblName.Text = _script.Name;
+            lblAssertType.Text = _script.Script_ExpectedResult[0].ExpectedResult.AssertType.Name;
+            lblAssertValue.Text = _script.Script_ExpectedResult[0].ExpectedResult.ExpectedValue;
+            isActiveCheckBox.Checked = _script.IsActive;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
