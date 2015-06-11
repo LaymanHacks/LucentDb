67c4b20a25b14e3eff59a18e2fbf8c09cfc81393
Mark Layman <MarkLayman@cox.net>
2015-06-11 06:54:38 -0500

    Add Project Summary sql

---
+++ ProjectSummary.sql
@@ -0,0 +1,4 @@
+﻿Select project.Name, count(test.id) over(partition by test.projectId) as TestCount, count(distinct test.groupid)  From project inner join test on test.ProjectId = Project.ProjectId left outer join TestGroup on  test.GroupId = TestGroup.Id
+where project.IsActive = 1
+ and test.IsActive = 1
+ group by project.Name, test.projectId, test.id
