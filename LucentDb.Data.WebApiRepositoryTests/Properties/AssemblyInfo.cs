54aad7c4d050128af086f260fc0c200a58ce1914
Mark Layman <Mark.l.layman@gmail.com>
2015-07-07 16:59:09 -0500

    Update test

--- LucentDb.Data.WebApiRepositoryTests/Properties/AssemblyInfo.cs
+++ LucentDb.Data.WebApiRepositoryTests/Properties/AssemblyInfo.cs
@@ -1,28 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("LucentDb.Data.RESTRepositoryTests")]
+[assembly: AssemblyTitle("LucentDb.Data.WebApiRepositoryTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LucentDb.Data.RESTRepositoryTests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Pulse Systems Inc")]
+[assembly: AssemblyProduct("LucentDb.Data.WebApiRepositoryTests")]
+[assembly: AssemblyCopyright("Copyright © Pulse Systems Inc 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("f637d992-acf9-4589-adf2-857efafdfdf6")]
+[assembly: Guid("5ab293c5-baf3-4c31-931e-a1abf58ac8e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- LucentDb.Data.WebApiRepositoryTests/WebApiTestRepositoryTests.cs
+++ LucentDb.Data.WebApiRepositoryTests/WebApiTestRepositoryTests.cs
@@ -1,156 +1,164 @@
-﻿using NUnit.Framework;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LucentDb.Data.WebApiRepository;
+using NUnit.Framework;
 namespace LucentDb.Data.WebApiRepository.Tests
 {
-    [TestFixture]
+    [TestFixture()]
     public class WebApiTestRepositoryTests
     {
-        [Test]
-        public void DeleteTest()
+        [Test()]
+        public void WebApiTestRepositoryTest()
         {
             Assert.Fail();
         }
 
-        [Test]
-        public void DeleteTest1()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
+        [Test()]
         public void DisposeTest()
         {
             Assert.Fail();
         }
 
-        [Test]
-        public void GetActiveDataByGroupIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataByGroupIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataByProjectIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataByProjectIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataByTestTypeIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataByTestTypeIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetActiveDataTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByGroupIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByGroupIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByProjectIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByProjectIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByTestTypeIdPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataByTestTypeIdTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void GetDataPageableTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
+        [Test()]
         public void GetDataTest()
         {
             Assert.Fail();
         }
 
-        [Test]
-        public void InsertTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void InsertTest1()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
+        [Test()]
         public void UpdateTest()
         {
             Assert.Fail();
         }
 
-        [Test]
+        [Test()]
         public void UpdateTest1()
         {
             Assert.Fail();
         }
 
-        [Test]
-        public void WebApiTestRepositoryTest()
+        [Test()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void InsertTest1()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByIdTest()
+        {
+            var repo = new WebApiTestRepository("http://localhost:60205/");
+          var tObject =  repo.GetDataById(1);
+            
+            Assert.AreNotEqual(0,tObject.Count);
+        }
+
+        [Test()]
+        public void GetActiveDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByProjectIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByProjectIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByProjectIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByProjectIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByGroupIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByGroupIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByGroupIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByGroupIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByTestTypeIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetDataByTestTypeIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByTestTypeIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetActiveDataByTestTypeIdPageableTest()
         {
             Assert.Fail();
         }
