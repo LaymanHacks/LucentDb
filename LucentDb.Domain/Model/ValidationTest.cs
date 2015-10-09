852351863e2507190ae3e79d1b5a004ccb2b93c0
Mark Layman <Mark.l.layman@gmail.com>
2015-10-09 13:03:05 -0500

    Add Validation Test

---
+++ LucentDb.Domain/Model/ValidationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LucentDb.Domain.Entities;
+
+namespace LucentDb.Domain.Model
+{
+    public class ValidationTest
+    {
+        public Connection Connection { get; set; }
+        public Test Test { get; set; }
+
+        public ValidationTest(Connection connection, Test test)
+        {
+            Connection = connection;
+            Test = test;
+
+        }
+    }
+}
