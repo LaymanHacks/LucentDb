0da23ba87cf2e1e9942738a31560d141b11af402
Mark Layman <Mark.l.layman@gmail.com>
2015-10-19 20:07:21 -0500

    Add missing file

---
+++ LucentDb.Domain/Model/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucentDb.Domain.Model
+{
+    public class Project : Entities.Project
+    {
+        private readonly ILucentDbRepositoryContext _dataRepository;
+
+        public Project(ILucentDbRepositoryContext dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        public bool ValidateConnection(int connectionId)
+        {
+            return
+                _dataRepository.ConnectionRepository.GetDataByProjectId(ProjectId)
+                    .Any(x => x.ConnectionId == connectionId);
+        }
+    }
+}
