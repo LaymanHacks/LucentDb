89445a551939950024e0e7559647b5b192a3a342
Mark Layman <MarkLayman@cox.net>
2015-11-01 09:57:18 -0600

    Add ValidationTestGroup

---
+++ LucentDb.Domain/Model/ValidationTestGroup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using LucentDb.Domain.Entities;
+using LucentDb.Validator.Model;
+
+namespace LucentDb.Domain.Model
+{
+    public class ValidationTestGroup
+    {
+        public ValidationTestGroup(Connection connection, TestGroup testGroup)
+        {
+            TestGroupId = testGroup.Id;
+            ValidationTests = new Collection<ValidationTest>();
+            foreach (var test in testGroup.Tests)
+            {
+                ValidationTests.Add(new ValidationTest(connection, test));
+            }
+        }
+
+        public int TestGroupId { get; private set; }
+        public bool IsValid { get; private set; }
+        public Collection<ValidationTest> ValidationTests { get; private set; }
+
+        public Collection<ValidationResponse> Validate()
+        {
+            var validationResponses = new Collection<ValidationResponse>();
+            var isValid = true;
+            foreach (var valResponse in ValidationTests.Select(vTest => vTest.Validate()))
+            {
+                isValid = valResponse.IsValid && isValid;
+                validationResponses.Add(valResponse);
+            }
+            IsValid = isValid;
+            return validationResponses;
+        }
+    }
+}
